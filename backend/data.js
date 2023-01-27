f9a4f01f372284225c2b756e4fe35257d4b02e47
Masixole <masixole.mtsikini@dadevs.co.za>
2023-01-27 14:32:23 +0200

    friday

---
+++ backend/data.js
@@ -0,0 +1,22 @@
+const data =[
+    {
+        url:"https://v16-webapp.tiktok.com/2c87ae5bbe18c79e5aff6f817ee3aa29/63c965fa/video/tos/useast2a/tos-useast2a-ve-0068c004/oQguJHN9HCXQ16kWAPCrebPj4AYhSR2jfIafM0/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=750&bt=375&cs=0&ds=3&ft=4b~OyMrD8Zmo0CP8464jVsWjZpWrKsdm&mime_type=video_mp4&qs=0&rc=aWdnOjk4Mzk5ODxlMzg6NkBpanh3dzU6Zjc5aTMzNzczM0BfNF5fL2A1NmAxLTRiXy42YSNqMWlvcjRnajBgLS1kMTZzcw%3D%3D&l=2023011909425106A8A07399DA869E4B8D&btag=80000",
+        channel: "Louzar",
+        description: "I'm warra warra shandice",
+        song: "the soul music",
+        likes : "231",
+        messages :"65",
+        shares : "212"
+    },
+    {
+        url:"https://v16-webapp.tiktok.com/2c87ae5bbe18c79e5aff6f817ee3aa29/63c965fa/video/tos/useast2a/tos-useast2a-ve-0068c004/oQguJHN9HCXQ16kWAPCrebPj4AYhSR2jfIafM0/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=750&bt=375&cs=0&ds=3&ft=4b~OyMrD8Zmo0CP8464jVsWjZpWrKsdm&mime_type=video_mp4&qs=0&rc=aWdnOjk4Mzk5ODxlMzg6NkBpanh3dzU6Zjc5aTMzNzczM0BfNF5fL2A1NmAxLTRiXy42YSNqMWlvcjRnajBgLS1kMTZzcw%3D%3D&l=2023011909425106A8A07399DA869E4B8D&btag=80000",
+        channel: "Zuma",
+        description: "Waste my hate on you",
+        song: "House gqom",
+        likes : "22231",
+        messages :"6225",
+        shares : "27812"
+    },
+];
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Videos from './dbModel.js'
+
+//app config
+const app = express();
+const port = 3003;
+
+//middleware
+app.use(express.json())
+
+//DB config
+
+const connection_url = "mongodb+srv://admin:P123456789@cluster0.ahdju3b.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+   // useCreateIndex: true,
+}).then(con =>{
+    console.log(`Mongodb database connected to ${port}`)
+});
+
+//API endpoints
+app.get('/', (req,res)=>res.status(200).send("Hello world"));
+
+app.get('/v1/posts', (req,res)=>{
+    Videos.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.post('/v2/posts', (req,res) => {
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data)=>{
+        if(err){
+            res.status(500).send(err)
+        } else{
+            res.status(201).send(data)
+        }
+    })
+});
+
+//Listener 
+app.listen(port, ()=> console.log(`listening on localhost:${port}`));
